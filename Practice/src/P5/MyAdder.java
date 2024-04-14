3dca2d4f9865f3fe2e2b808947d512cb3633b432
S-SIRIUS <sirius5b8b@gmail.com>
2024-04-15 02:51:30 +0900

    [feat]class

---
+++ Practice/src/P5/MyAdder.java
@@ -0,0 +1,21 @@
+package P5;
+
+class AdderInterface{
+
+        public int add(int x, int y){
+            return x+y;
+        }
+        public int add(int n){
+            int sum=0;
+            for(int i=1; i<=n; i++)
+                sum+=i;
+            return sum;
+        }
+}
+public class MyAdder extends AdderInterface{
+    public static void main(String[] args){
+        MyAdder adder = new MyAdder();
+        System.out.println(adder.add(5, 10));
+        System.out.println(adder.add(10));
+    }
+}
---
+++ Practice/src/P5/StringStack.java
@@ -0,0 +1,49 @@
+package P5;
+import java.util.Scanner;
+class StackInterface{
+    String [] stack = new String[5];
+    public int length(){
+        return stack.length;
+    }
+    public String pop(){
+        for(int i=4; i>=0; i--){
+            if(stack[i]!=null){
+                String ans = stack[i];
+                stack[i] = null;
+                return ans;
+            }
+        }
+        return null;
+    }
+    public boolean push(String st){
+        for(int i=0; i<5; i++){
+            if(stack[i]==null){
+                stack[i] = st;
+                return true;
+            }
+        }
+        return false;
+    }
+}
+public class StringStack extends StackInterface{
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(">>");
+        String test = scanner.nextLine();
+        String [] st = test.split(" ");
+        StringStack stack = new StringStack();
+
+        System.out.println(test);
+        for(int i=0; i<5; i++)
+        {
+            stack.push(st[i]);
+        }
+        for(int i=0; i<5; i++)
+        {
+            System.out.print(stack.pop()+" ");
+        }
+
+
+    }
+}
