6dc3a8cba49f1f71692c0e65c9f4e83ceaf73528
S-SIRIUS <sirius5b8b@gmail.com>
2024-04-22 19:53:40 +0900

    [feat] testing

---
+++ Practice/src/c_test.java
@@ -0,0 +1,18 @@
+class p{
+    public String name;
+
+    public p(String name){
+        this.name = name;
+    }
+}
+
+class CCg extends p{
+    public int std_num;
+
+    public CCg(String name, int num){
+        this.name=name;
+        this.std_num = num;
+    }
+}
+public class c_test {
+}
--- Practice/src/test2.java
+++ Practice/src/test2.java
@@ -1,19 +1,23 @@
-import java.util.Scanner;
-public class test2 {
-    public static void main(String [] args){
-        Scanner scanner = new Scanner(System.in);
-        int choose = scanner.nextInt();
-        while(true) {
-            switch (choose) {
-                case 1:
-                    int a = scanner.nextInt();
-                case 2:
-                    int b = scanner.nextInt();
-                case 3:
+class AA{
+    protected int field1=0;
+    protected void method(){
+        System.out.println("부모메소드");
+    }
+}
+public class test2 extends AA{
 
-            }
-        }
-
+    public void method(){
+        System.out.println("자식으로 오버라이딩");
+    }
+    public void method2(){
+        this.field1 = 10;
+        super.method();
+        this.method();
     }
 
+    public static void main(String[] args){
+        test2 t1 = new test2();
+        t1.method2();
+        System.out.print(t1.field1);
+    }
 }
